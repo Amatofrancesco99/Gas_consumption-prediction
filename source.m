18c312e1192c309dcc4b80b78b237d76e77553f4
AmatoFrancesco99 <francesco.amato01@universitadipavia.it>
2021-04-12 11:00:37 +0200

    added plot

--- source.m
+++ source.m
@@ -1,6 +1,3 @@
-%% CONSEGNA/INTRODUZIONE
-
-
 %% ANNOTAZIONE SUL CODICE 
 
 
@@ -17,7 +14,32 @@ close all;
 %% SVOLGIMENTO
 % Importo la tabella, selezionando solo i dati a cui sono interessato a
 % visualizzare
-ds=readtable('./Dataset/gasITAday.xlsx', 'Range', 'A8:C732');
+dsAnno1=readtable('./Dataset/gasITAday.xlsx', 'Range', 'A3:C367');
+dsAnno2=readtable('./Dataset/gasITAday.xlsx', 'Range', 'A368:C732');
+
+% Modifico i nomi delle colonne del dataset
+dsAnno1.Properties.VariableNames{1}='GiornoAnno';
+dsAnno1.Properties.VariableNames{2}='GiornoSettimana';
+dsAnno1.Properties.VariableNames{3}='Dati';
+
+dsAnno2.Properties.VariableNames{1}='GiornoAnno';
+dsAnno2.Properties.VariableNames{2}='GiornoSettimana';
+dsAnno2.Properties.VariableNames{3}='Dati';
+
+%Plottiamo i grafici dei due anni
+%Anno1
+figure(1);
+subplot(2,1,1);
+plot(dsAnno1.GiornoAnno,dsAnno1.Dati, 'Linewidth' , 2);
+xlabel('Days of Year 1');
+ylabel('Consumption (millionM^3)');
+grid on
+%Anno2
+subplot(2,1,2);
+plot(dsAnno2.GiornoAnno,dsAnno2.Dati, 'r', 'Linewidth' , 2);
+xlabel('Days of Year 2');
+ylabel('Consumption (millionM^3)');
+grid on
 
 %% CONCLUSIONE
 
