ac0f30f55e6416a58b38d22d75cd3c96237e795b
Amatofrancesco99 <80333091+Amatofrancesco99@users.noreply.github.com>
2021-04-14 15:26:43 +0200

    Update plottingGraphs.m
    Added new observations inherent 2D Graphs (of Gas Consumption)

--- Scripts/plottingGraphs.m
+++ Scripts/plottingGraphs.m
@@ -38,7 +38,7 @@ for i = 1:12
        xline(i*30, 'm--');
 end
 
-% FIRST OBSERVATIONS:
+% FIRST (EASY) OBSERVATIONS:
 % From the diagrams we can already notice that the gas consumption decreases 
 % in the hottest months of the year, more specifically in the days that go 
 % from the 120th (April) to the 290th (September).
@@ -52,6 +52,29 @@ end
 % the residential and commercial sectors. 
 % In the summer months, warm weather leads to more demand for air conditioning 
 % and, in turn, more demand for electricity.
+
+% SOME MATHEMATICAL OBSERVATIONS:
+% From the 2D graph of the two signals (gas consumption, depending on the day 
+% of the year and the day of the week), we can also notice that it is a discrete
+% time stationary signal.
+% Stationary as if we took two sufficiently distant windows we can see that 
+% the trend seems to be similar (except for small oscillations due to noise),
+% more specifically if we consider windows spaced 6 months concatenating the
+% two years (taking December of year 1 and chaining it with January of year 2).
+% Discrete time because the realizations have been taken only once a day for
+% a year and not for every moment of time of every day of a year (reasonable 
+% enough because we do not care to know the gas consumption per second, but 
+% in a whole day). 
+% We can also notice that the signal sign stay positive (January-April)
+% then falls (April-September), then goes higher (Semptember-April_Year 2).
+% Finally we can say that its autocovariance function will be centered around
+% an average value (average gas consumption in a year, which corresponds to 
+% the sum of daily gas consumption in a year / the number of days in a year), 
+% and will have a fairly slow decline (because it remains positive for 6 
+% months, then it goes down). 
+% So its Fourier transform (of the autocovariance) will be a fairly "fat"
+% and "low" Gaussian (slow decay and always centered around the average value 
+% previously mentioned).
 
 
 % PLOTTING DATASET ON 3D GRAPHS, for each year
