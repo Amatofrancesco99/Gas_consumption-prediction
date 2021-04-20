58208f84efc4476abe51e0f55e9d9e37983923d8
AmatoFrancesco99 <francesco.amato01@universitadipavia.it>
2021-04-20 19:16:30 +0200

    Added gas consumption prediction, using best model (Neural network in our case)

--- Scripts/predictionWithValuesEntry.m
+++ Scripts/predictionWithValuesEntry.m
@@ -12,21 +12,83 @@ while (END==0)
   % Insert and Control of: Day of the week and Day of the year Variables
   % to than predict gas consumes
   while (DayOfTheWeek<=0 || DayOfTheWeek>7)
-      fprintf('Insert the day of the week (it must be an integer)');
-      fprintf('(Remember that 1 is Sunday, 7 is Saturday)');
-      DayOfTheWeek=str2int(input());
+      fprintf('\nInsert the day of the week (it must be an integer)');
+      fprintf('\n(Remember that 1 is Sunday, 7 is Saturday)\n ');
+      DayOfTheWeek=input('');
   end
   while ((DayOfTheYear<=0 || DayOfTheYear>365))
-      fprintf('Insert the day of the year (it must be an integer, from 1 to 365)');
-      DayOfTheYear=str2int(input());
+      fprintf('\nInsert the day of the year \n(it must be an integer, from 1 to 365)\n ');
+      DayOfTheYear=input('');
   end
   
   % Gas consumption prediction using best model
-
+  % Before running this script run "mlpNN" script, because you need the
+  % MLPNN_net to predict the gas consumption values
+  fprintf('\nGas consumption predicted value\n ');
+  s_hat = prediz(DayOfTheYear,DayOfTheWeek)
+  
+  
   % Ask if you want to continue/end insert new values and predicting gas
   % consumes
-  fprintf('You want to end the prediction of gas consumption inserting new values of day')
-  fprintf('of the week and day of the year?');
-  fprintf('(0=continue, 1=end)');
-  END=str2int(input());
+  fprintf('\nYou want to end the prediction of gas consumption inserting new values \nof day')
+  fprintf(' of the week and day of the year?');
+  fprintf(' (0=continue, 1=end)\n ');
+  END=input('');
+  if (END==0) 
+      DayOfTheYear=0;
+      DayOfTheWeek=0;
+  end   
+end
+
+
+% Stop to see the results
+pause
+% Clear terminal
+clc;
+
+
+%% PREDICTION FUNCTION, USING NEURAL NETWORK
+
+% Identify neural network model
+function net =identifyMLP_net()
+    % input dataset for the neural network 
+    % (all the DayOfTheYear-DayOfTheWeek data, both years)
+    inputDatasetNN = table2array(readtable('../Dataset/gasITAday.xlsx', 'Range', 'A3:B732'));
+
+    % output (target) dataset for the neural network
+    % (all the gas consumption data, both years)
+    outputDatasetNN = table2array(readtable('../Dataset/gasITAday.xlsx', 'Range', 'C3:C732'));
+
+    %   inputDatasetNN - input data.
+    %   outputDatasetNN - target data.
+
+    x = inputDatasetNN';
+    t = outputDatasetNN';
+
+    % Choose a Training Function
+    trainFcn = 'trainbr';  % Bayesian Regularization backpropagation.
+
+    % Create a Fitting Network
+    hiddenLayerSize = 10;
+    MLPNN_net = fitnet(hiddenLayerSize,trainFcn);
+
+    % Setup Division of Data for Training, Validation, Testing
+    net.divideParam.trainRatio = 70/100;
+    net.divideParam.valRatio = 15/100;
+    net.divideParam.testRatio = 15/100;
+
+    % Train the Network
+    [MLPNN_net,tr] = train(MLPNN_net,x,t);
+    
+    % Test the Network
+    y = MLPNN_net(x);
+    
+    net=MLPNN_net;
+end
+
+% Predicting gas consumes
+function f = prediz(d,w)
+    %Identify the network
+    net=identifyMLP_net();
+    f = net([d;w]);
 end
