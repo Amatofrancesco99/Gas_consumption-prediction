5e1e5c11e3ce94c9c7dd44018127224be00b395e
Amatofrancesco99 <80333091+Amatofrancesco99@users.noreply.github.com>
2021-05-18 21:24:28 +0200

    Update prediz.m

--- Scripts/prediz.m
+++ Scripts/prediz.m
@@ -4,6 +4,6 @@ function s_hat=prediz(dy,dw)
     x=dy;
     x2=dw;
     % Second degree harmonic function
-    % The numeric value of the coefficient terms are the same one we've found in live_script.mlx
+    % The numeric value of the coefficient terms are the same one we found in "live_script.mlx" code
     s_hat=92.335926898759130 + 81.438247639981800*cos(x*((2*pi)/365)) + 34.239314268549640*sin(x*((2*pi)/365)) + -6.681112039616750*cos(x2*((2*pi)/7)) + -2.675412560965765*sin(x2*((2*pi)/7))+ 17.885671526017582*cos(x*((4*pi)/365)) + 17.376574512281540*sin(x*((4*pi)/365)) + -2.382534793449163*cos(x2*((4*pi)/7)) + -3.929506835007748*sin(x2*((4*pi)/7));
 end
